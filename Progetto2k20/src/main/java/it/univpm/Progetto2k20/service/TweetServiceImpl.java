9161a99de0fb8ee0cf34f8b2098256460ba524fb
andreeacraiu <64148015+andreeacraiu@users.noreply.github.com>
2020-07-08 20:33:20 +0200

    Update TweetServiceImpl.java

--- Progetto2k20/src/main/java/it/univpm/Progetto2k20/service/TweetServiceImpl.java
+++ Progetto2k20/src/main/java/it/univpm/Progetto2k20/service/TweetServiceImpl.java
@@ -5,33 +5,67 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Service;
+import it.univpm.Progetto2k20.utils.*;
 import it.univpm.Progetto2k20.model.*;
-import it.univpm.Progetto2k20.utils.JSONDownloader;
-import it.univpm.Progetto2k20.utils.JSONParse;
+
+/**
+ * Classe che implementa i metodi dell'interfaccia TweetService
+ * 
+ * @see TweetService
+ *
+ */
+
 
 
 @Service
 public class TweetServiceImpl implements TweetService {
 
-	
-	private Map<Long, Tweet> timeline=new HashMap<>();
+	private Map<String, Metadata> metadata = new HashMap<>();
+	private Map<Long, Tweet> data=new HashMap<>();
+	private Stat stat = UploadStat.getStat();
+
 	
 	public TweetServiceImpl() {
 		try {
-			ArrayList<Tweet> twe= new ArrayList<Tweet>();
-			twe = JSONParse.parsaJson(JSONDownloader.download());
+			ArrayList<Tweet> twe = JSONParse.parsaJson(JSONObject.downloadTweets());
 			for (int i = 0; i < twe.size(); i++) {
 				Tweet tw = twe.get(i);
-				timeline.put((long) tw.getId(), tw);
+				data.put((long) tw.getId(), tw);
 			}
+			
 		
-		} catch (Exception e) {
+			ArrayList<Metadata> metadati = Metadati.getMetadata();
+			for (int i = 0; i < metadati.size(); i++) {
+				Metadata met = metadati.get(i);
+				metadata.put ((String) met.getName(), met);
+		
+			} } catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 	}
-	public Collection<Tweet> getTweet() {
-		return timeline.values();
+	
+	
+
+	public Collection<Tweet> getTweets() {
+		return data.values();
 	}
+	
+	
+	@Override
+	public Collection<Metadata> getMetadata() {
+		return metadata.values();
+	}
+	
+    @Override
+	public Stat getStat() {
+		return stat;
+	}
+	
+	
+	
+	
+	
 }
+
 	

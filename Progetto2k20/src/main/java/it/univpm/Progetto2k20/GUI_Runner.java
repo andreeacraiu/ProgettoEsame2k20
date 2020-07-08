c9205570e5e0a8f41ad320846e88895c2e792ef0
andreeacraiu <64148015+andreeacraiu@users.noreply.github.com>
2020-07-08 20:40:13 +0200

    main GUI

---
+++ Progetto2k20/src/main/java/it/univpm/Progetto2k20/GUI_Runner.java
@@ -0,0 +1,15 @@
+package it.univpm.Progetto2k20;
+
+import it.univpm.Progetto2k20.GUI.*;
+
+/**
+ * Classe contenente il main dell'interfaccia grafica
+ * 
+ *
+ */
+public class GUI_Runner {
+	public static void main(String[] args) {
+		Graphical_Interface gui = new Graphical_Interface();
+	}
+
+}

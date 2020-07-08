ecc9295c1c41f1a4106e175a08367bd96ccc2b39
andreeacraiu <64148015+andreeacraiu@users.noreply.github.com>
2020-07-08 20:32:29 +0200

    Update TweetService.java

--- Progetto2k20/src/main/java/it/univpm/Progetto2k20/service/TweetService.java
+++ Progetto2k20/src/main/java/it/univpm/Progetto2k20/service/TweetService.java
@@ -3,13 +3,28 @@ package it.univpm.Progetto2k20.service;
 import java.util.Collection;
 
 import org.springframework.stereotype.Service;
-
 import it.univpm.Progetto2k20.model.*;
+
+/**
+ * Interfaccia che contiene i metodi che saranno implementati in
+ * TweetServiceImpl
+ * 
+ * @see TweetServiceImpl
+ *
+ */
 
 @Service
 public interface TweetService {
 
-	//public abstract Stat getStats();
-	public Collection<Tweet> getTweet();
+	public abstract Stat getStat();
+
+	public Collection <Tweet> getTweets();
+	
+	
+	
+	public abstract Collection<Metadata> getMetadata();
+	
+	
 }
+
 
